d4358e3fbe783d723f6df751cef14be60589e684
helmi-source <sikwn8726@gmail.com>
2026-03-11 20:12:50 +0800

    server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 3000;  // PENTING: Render pake PORT dari environment
 
 // === PENTING: BIARIN RENDER BISA SERVE FILE HTML ===
-app.use(express.static('public'));
+app.use(express.static(__dirname));
 // ==================================================
 
 app.get('/cek-region', async (req, res) => {
@@ -31,4 +31,5 @@ app.get('/cek-region', async (req, res) => {
 
 app.listen(port, '0.0.0.0', () => {
     console.log(`[BACKEND] Online on port ${port}`);
+
 });
